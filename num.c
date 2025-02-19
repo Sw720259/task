4a6551e4d0f121951c04f63d6e7cc521c2c1f16b
rekha mahesh <rekha.m@s.amity.edu>
2025-02-19 20:34:02 +0530

    largest of two numbers

---
+++ num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int maun(){
+	int num1=10,num2=5,largest;
+	if(num1>num2){
+		largest=num1;
+		printf("the largest of number1.%d \n",largest);
+	}
+	else
+	{
+		largest=num2;
+		printf("the largest of number2.%d \n",largest);
+	}
+
+	return 0;
+}
