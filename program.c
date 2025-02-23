9c91934cb48f3602e761e33b4bd9fff24f9314a8
rekha mahesh <rekha.m@s.amity.edu>
2025-02-23 14:19:18 +0530

    print the valid day

--- program.c
+++ program.c
@@ -35,3 +35,36 @@ int main(){
 
 	return 0;
 }
+#include<stdio.h>
+int main(){
+	int day;
+	printf("enter a number(1-7):");
+	sacnf("%d",&day);
+	switch(day){
+		case 1:
+			printf("sunday\n");
+			break;
+		case 2:
+			printf("monday\n");
+			break;
+		case 3:
+			printf("tuesday\n");
+			break;
+		case 4:
+			printf("wednesday\n");
+			break;
+		case 5:
+			printf("thrusday\n");
+			break;
+		case 6:
+			printf("friday\n");
+			break;
+		case 7:
+			printf("saturday\n");
+			break;
+		case 8:
+			printf("invalid day:\n");
+			break;
+	}
+	return 0;
+}
