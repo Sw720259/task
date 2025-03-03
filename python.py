3a14fb02530cc31310e2c3872bca51a3f963930a
rekha mahesh <rekha.m@s.amity.edu>
2025-03-03 21:16:29 +0530

    print pattern

--- python.py
+++ python.py
@@ -5,3 +5,10 @@ x=(a+b)/(b*3)
 print("the x is:{}".format(x))
 m=(a>=b) and (c!=c)
 print("the m is :{}".format(m))
+
+
+for i in range(1,5):
+    for j in range(i):
+    print('*',end=' ')
+ print()
+
