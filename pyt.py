f8707f76682f1a3111827b296c950108240e1a0d
rekha mahesh <rekha.m@s.amity.edu>
2025-03-03 21:44:42 +0530

    print the kilometer to miles

--- pyt.py
+++ pyt.py
@@ -2,6 +2,13 @@ num=6
 fact=1;
 for i in range(1,num+i)
  fact=*i
- print("the factorial of %d=%d.%(num,fact))
+ print("the factorial of %d=%d".%(num,fact))
+
+
+       def my_miles(k:int):
+       return(k*0.621371)
+       k=250
+       result=my_miles(k);
+       print(f"the miles is:{result}")
 
 
