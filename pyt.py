82467deb42d2ee822c583a2b707a4ef952e864bb
rekha mahesh <rekha.m@s.amity.edu>
2025-03-03 21:51:54 +0530

    print the list elements

--- pyt.py
+++ pyt.py
@@ -12,3 +12,9 @@ for i in range(1,num+i)
        print(f"the miles is:{result}")
 
 
+list 1=['physics','chemistry',1997,2000];
+ list 2=[1,2,3,4,5,6,7];
+ print("list1[0]:",list1[0])
+ print("list2[0:5]:",list2[0:2])
+
+
