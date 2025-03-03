a237e033ef810cf35b4d790bdf0aaa4281887100
rekha mahesh <rekha.m@s.amity.edu>
2025-03-03 22:08:06 +0530

    print the graph

--- pyth.py
+++ pyth.py
@@ -4,4 +4,11 @@ print("before append",number)
 number.append(32)
 print("after append",number)
 
+  
+
+  x points=np.array([1,8])
+  y ponts=np.array([1,10])
+  plt.plot(x points,y points)
+  plt.show()
+
 
